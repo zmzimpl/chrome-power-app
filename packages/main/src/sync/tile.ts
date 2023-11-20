4f6a557b214c8980da9a99f8a11c131fa1cff213
zmzimpl <zmzimpl@gmail.com>
2023-11-20 21:10:41 +0800

    test workflow to release

--- packages/main/src/sync/tile.ts
+++ packages/main/src/sync/tile.ts
@@ -2,14 +2,15 @@ import type {SafeAny} from '../../../shared/types/db';
 
 let windowAddon: unknown;
 
-if (process.env.NODE_ENV === 'development') {
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'production') {
+  windowAddon = require('../native-addon/build/Release/window-addon.node');
+} else {
   const path = require('path');
   windowAddon = require(path.join(
     __dirname,
     '../src/native-addon/build/Release/window-addon.node',
   ));
-} else {
-  windowAddon = require('../native-addon/build/Release/window-addon.node');
 }
 export const tileWindows = async () => {
   (windowAddon as unknown as SafeAny)!.tileChromeWindows();

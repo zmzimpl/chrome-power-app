b08a99ae73099629692e6efceae7444d753bfaf7
LiWeiJie <3258926778@qq.com>
2025-03-14 10:20:17 +0800

    查看窗口的搜索框的或查询功能

--- packages/renderer/src/utils/str.ts
+++ packages/renderer/src/utils/str.ts
@@ -1,4 +1,4 @@
 export const containsKeyword = (str: string | number | undefined, keyword: string): boolean => {
-  if (typeof str === 'undefined') return false;
-  return str?.toString()?.toLowerCase()?.includes(keyword.toLowerCase());
+  if (typeof str === 'undefined') return false; // 如果 str 是 undefined，返回 false
+  return str?.toString()?.toLowerCase()?.includes(keyword.toLowerCase()); // 检查 str 是否包含关键字
 };
